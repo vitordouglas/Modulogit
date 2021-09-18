f0450b08f5ccb64ffde814bc4a2717e45fb0e4f9
Douglas Vitor <dogvicfari@gmail.com>
2021-09-18 15:09:58 -0300

    mudando as fotos no js

--- Master/script.js
+++ Master/script.js
@@ -15,31 +15,31 @@ function verificar() {
           genero = 'Masculino'
           if(idade >=0 && idade < 15) {
             //Criança
-            img.setAttribute('src', 'bb-menino.png')
+            img.setAttribute('src', 'Master/bb-menino.png')
           } else if(idade >= 15 && idade < 21) {
             //Jovem
-            img.setAttribute('src', 'jovem-adulto.png')
+            img.setAttribute('src', 'Master/jovem-adulto.png')
           } else if(idade < 50) {
             //Adulto
-            img.setAttribute('src', 'adulto-homem.png')
+            img.setAttribute('src', 'Master/adulto-homem.png')
           } else {
             //idoso
-            img.setAttribute('src', 'idoso.png')
+            img.setAttribute('src', 'Master/idoso.png')
           }
         } else if (fsex[1].checked) {
           genero = 'Femenino'
           if(idade >=0 && idade < 15) {
             //Criança
-            img.setAttribute('src', 'menina.png')
+            img.setAttribute('src', 'Master/menina.png')
           } else if(idade >= 15 && idade < 21) {
             //Jovem
-            img.setAttribute('src', 'jovem-mulher.png')
+            img.setAttribute('src', 'Master/jovem-mulher.png')
           } else if(idade < 50) {
             //Adulto
-            img.setAttribute('src', 'adulta-mulher.png')
+            img.setAttribute('src', 'Master/adulta-mulher.png')
           } else {
             //idoso
-            img.setAttribute('src', 'idosa.png')
+            img.setAttribute('src', 'Master/idosa.png')
           }
         }
         res.style.textAlign = 'center'
